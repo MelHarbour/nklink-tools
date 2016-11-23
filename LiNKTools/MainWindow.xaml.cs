b058fc34039bd5ce8f232fc498fb5f92b5cceb9b
Melvyn Harbour <melharbour@gmail.com>
2016-11-23 11:38:46 +0000

    Dummy connection to the database

--- LiNKTools/MainWindow.xaml.cs
+++ LiNKTools/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,35 @@ namespace LiNKTools
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".sqlite";
+            openFileDialog.Filter = "SQLite Databases (*.sqlite)|*.sqlite";
+
+            if (openFileDialog.ShowDialog() == true)
+                textBoxFilePath.Text = openFileDialog.FileName;
+        }
+
+        private void buttonTestConnection_Click(object sender, RoutedEventArgs e)
+        {
+            DbProviderFactory fact = DbProviderFactories.GetFactory("System.Data.SQLite");
+            using (SQLiteConnection cnn = (SQLiteConnection)fact.CreateConnection())
+            {
+                cnn.ConnectionString = "Data Source=" + textBoxFilePath.Text;
+                cnn.Open();
+
+                SQLiteCommand command = new SQLiteCommand(
+                    "SELECT * FROM SpeedCoaches", cnn);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                Console.WriteLine("Rows returned: " + reader.HasRows);
+
+                reader.Close();
+            }
         }
     }
 }
